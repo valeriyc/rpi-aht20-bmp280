051aef4748019d7ce569e5495cbc6b3c184a3f39
Valerii Chebanov <valeriy.chebanov@gmail.com>
2024-12-31 20:57:56 +0200

    Added aht20 humidity and temperature monitoring.

--- init-runtime.sh
+++ init-runtime.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 apt-get update && apt-get -y install python3 python3-pip
-pip3 install smbus2 RPi.bme280
+pip3 install smbus2 bmp280
--- read-temp.py
+++ read-temp.py
@@ -1,4 +1,6 @@
 import time
+import adafruit_ahtx0
+import board
 
 from smbus2 import SMBus
 from bmp280 import BMP280
@@ -11,6 +13,9 @@ address = 0x77
 bus = SMBus(port)
 bmp280 = BMP280(i2c_addr=address,i2c_dev=bus)
 
+aht20 = adafruit_ahtx0.AHTx0(board.I2C())
+
+
 def print_gauge(name, value, text):
     print(f'# HELP {name} {text}')
     print(f'# TYPE {name} gauge')
@@ -19,6 +24,7 @@ def print_gauge(name, value, text):
 
 # the compensated_reading class has the following attributes
 print_gauge("temperature", bmp280.get_temperature(), "Temperature in celsius")
+print_gauge("temperature_aht20", aht20.temperature, "Temperature in celsius")
 print_gauge("pressure", bmp280.get_pressure(), "Pressure")
-print_gauge("humidity", "N/A", "Humidity percentage")
+print_gauge("humidity", aht20.relative_humidity, "Humidity percentage")
 
